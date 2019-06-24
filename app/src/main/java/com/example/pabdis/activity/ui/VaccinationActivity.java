205dae24643435937621e992a0acc27bcbdc9b14
Jeannie Aquino <jeanniebethdnlx@gmail.com>
2019-06-24 15:49:50 +0800

    Signed-off-by: Jeannie Aquino <jeanniebethdnlx@gmail.com>

--- app/src/main/java/com/example/pabdis/activity/ui/VaccinationActivity.java
+++ app/src/main/java/com/example/pabdis/activity/ui/VaccinationActivity.java
@@ -1,6 +1,25 @@
 package com.example.pabdis.activity.ui;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.example.pabdis.R;
+import com.example.pabdis.activity.survey.ChickenActivity;
+import com.example.pabdis.activity.survey.SwineActivity;
 
 public class VaccinationActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vacc);
+
+
+
+    }
 }
