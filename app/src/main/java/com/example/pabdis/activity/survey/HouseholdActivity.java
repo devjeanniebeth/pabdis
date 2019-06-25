1f75324c338e9322ca5693daa0731f33a3d92a4f
Jeannie Aquino <jeanniebethdnlx@gmail.com>
2019-06-25 17:03:51 +0800

    Signed-off-by: Jeannie Aquino <jeanniebethdnlx@gmail.com>

--- app/src/main/java/com/example/pabdis/activity/survey/HouseholdActivity.java
+++ app/src/main/java/com/example/pabdis/activity/survey/HouseholdActivity.java
@@ -7,22 +7,35 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.pabdis.R;
+import com.example.pabdis.activity.helper.DatabaseHelper;
 import com.example.pabdis.activity.ui.MainActivity;
 import com.example.pabdis.activity.ui.VaccinationActivity;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class HouseholdActivity extends AppCompatActivity {
 
 
     Button btnDone;
     String ownerid;
+    DatabaseHelper myDB;
+    EditText edtBeef,edtCarabeef,edtPork,edtChicken,edtFish,edtEgg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survey_household);
         btnDone = findViewById(R.id.btnProceedSurvey);
+        edtBeef = findViewById(R.id.edtBeef);
+        edtCarabeef = findViewById(R.id.edtCarabeef);
+        edtPork = findViewById(R.id.edtPork);
+        edtChicken = findViewById(R.id.edtChicken);
+        edtFish = findViewById(R.id.edtFish);
+        edtEgg = findViewById(R.id.edtEgg);
 
         if (savedInstanceState == null) {
             Bundle extras = getIntent().getExtras();
@@ -38,6 +51,21 @@ public class HouseholdActivity extends AppCompatActivity {
         btnDone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                final String beef = edtBeef.getText().toString();
+                final String carabeef = edtCarabeef.getText().toString();
+                final String pork = edtPork.getText().toString();
+                final String chicken = edtChicken.getText().toString();
+                final String fish = edtFish.getText().toString();
+                final String egg = edtEgg.getText().toString();
+
+
+
+                Calendar cal = Calendar.getInstance();
+                cal.add(Calendar.DATE, 1);
+                SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd");
+                final String created_at = format1.format(cal.getTime());
+
 
                 // Build an AlertDialog
                 AlertDialog.Builder builder = new AlertDialog.Builder(HouseholdActivity.this);
@@ -55,16 +83,28 @@ public class HouseholdActivity extends AppCompatActivity {
                         switch(which){
                             case DialogInterface.BUTTON_POSITIVE:
                                 // User clicked the Yes button
-                                Intent intent = new Intent(getApplicationContext(), VaccinationActivity.class);
-//                                intent.putExtra("owner_id",ownerid);
-                                startActivity(intent);
+
+                                if (beef.equals("") || carabeef.equals("") || pork.equals("") ||
+                                        chicken.equals("") || fish.equals("") || egg.equals("")) {
+                                    Toast.makeText(HouseholdActivity.this, "Check your input!" , Toast.LENGTH_SHORT).show();
+                                }else {
+                                    try {
+                                        myDB.addHousehold(ownerid, beef, carabeef, pork,chicken,fish,egg,created_at);
+                                        Intent intent = new Intent(getApplicationContext(), VaccinationActivity.class);
+                                        intent.putExtra("owner_id", ownerid);
+                                        startActivity(intent);
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+
+                                    }
+                                }
+
                                 break;
 
                             case DialogInterface.BUTTON_NEGATIVE:
                                 // User clicked the No button
-
-                                 intent = new Intent(getApplicationContext(), MainActivity.class);
-//                                intent.putExtra("owner_id",ownerid);
+                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                intent.putExtra("owner_id",ownerid);
                                 startActivity(intent);
                                 break;
                         }
--- app/src/main/java/com/example/pabdis/activity/ui/VaccinationActivity.java
+++ app/src/main/java/com/example/pabdis/activity/ui/VaccinationActivity.java
@@ -1,30 +1,47 @@
 package com.example.pabdis.activity.ui;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.pabdis.R;
 import com.example.pabdis.activity.survey.ChickenActivity;
 import com.example.pabdis.activity.survey.SwineActivity;
 
+import java.io.ByteArrayOutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
 
 public class VaccinationActivity extends AppCompatActivity {
 
 
-    EditText dateSurvey;
-    Button btndate;
-    Calendar myCalendar;
+    EditText dateSurvey, otherbreed, othercolormark, txtAge;
+    Button btndate, chooseImg;
+    final Calendar myCalendar = Calendar.getInstance();
+    ImageView imgView;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    public  static final int RequestPermissionCode  = 1 ;
+    Spinner txtbreed, txtGender, txtSpecie, txtColorMark;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +51,27 @@ public class VaccinationActivity extends AppCompatActivity {
 
         dateSurvey = findViewById(R.id.edtDateSurveyed);
         btndate = findViewById(R.id.btnDate);
+
+
+        chooseImg = findViewById(R.id.btnChoose);
+        imgView = findViewById(R.id.imgPet);
+        txtbreed = findViewById(R.id.txtbreed);
+        txtGender = findViewById(R.id.txtgender);
+        txtSpecie = findViewById(R.id.txtspecies);
+        txtColorMark = findViewById(R.id.txtcolormark);
+        otherbreed = findViewById(R.id.txtbreedother);
+        othercolormark = findViewById(R.id.txtcolorother);
+        txtAge = findViewById(R.id.txtage);
+
+        otherbreed.setVisibility(View.GONE);
+        othercolormark.setVisibility(View.GONE);
+
+
+
+        EnableRuntimePermissionToAccessCamera();
+
+        final ArrayAdapter<CharSequence> adapter =  ArrayAdapter.createFromResource(this, R.array.breed_dod, R.layout.support_simple_spinner_dropdown_item);
+        final ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.breed_cat, R.layout.support_simple_spinner_dropdown_item);
 
 
 
@@ -47,9 +85,12 @@ public class VaccinationActivity extends AppCompatActivity {
                 myCalendar.set(Calendar.MONTH, monthOfYear);
                 myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
                 updateLabel();
+                txtAge.setText(Integer.toString(calculateAge(myCalendar.getTimeInMillis())));
             }
 
         };
+
+
 
         btndate.setOnClickListener(new View.OnClickListener() {
 
@@ -60,12 +101,141 @@ public class VaccinationActivity extends AppCompatActivity {
                         myCalendar.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
+
+        txtSpecie.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String selectedItem = adapterView.getItemAtPosition(i).toString();
+
+
+                if(selectedItem.equals("Cat")) {
+                    txtbreed.setAdapter(adapter2);}
+                else if(selectedItem.equals("Dog")) {
+                    txtbreed.setAdapter(adapter);}
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        txtColorMark.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = parent.getItemAtPosition(position).toString();
+
+                if(selectedItem.equals("Others")) {
+                    othercolormark.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+        txtbreed.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = parent.getItemAtPosition(position).toString();
+                if(selectedItem.equals("Others")) {
+                    otherbreed.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        chooseImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
+
     }
 
     private void updateLabel() {
         String myFormat = "MM/dd/yyyy"; //In which you need put here
         SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
-
         dateSurvey.setText(sdf.format(myCalendar.getTime()));
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    int calculateAge(long date){
+        Calendar dob = Calendar.getInstance();
+        dob.setTimeInMillis(date);
+        Calendar today = Calendar.getInstance();
+        int age = today.get(Calendar.YEAR) - dob.get(Calendar.YEAR);
+        if(today.get(Calendar.DAY_OF_MONTH) < dob.get(Calendar.DAY_OF_MONTH)){
+            age--;
+        }
+        return age;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int RC, String per[], int[] PResult) {
+
+        switch (RC) {
+
+            case RequestPermissionCode:
+
+                if (PResult.length > 0 && PResult[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Toast.makeText(VaccinationActivity.this,"Permission Granted, Now your application can access CAMERA.", Toast.LENGTH_LONG).show();
+
+                } else {
+
+                    Toast.makeText(VaccinationActivity.this,"Permission Cancelled, Now your application cannot access CAMERA.", Toast.LENGTH_LONG).show();
+
+                }
+                break;
+        }
+    }
+
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imgView.setImageBitmap(imageBitmap);
+        }
+    }
+
+    public void EnableRuntimePermissionToAccessCamera() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(VaccinationActivity.this, Manifest.permission.CAMERA)) {
+
+            Toast.makeText(VaccinationActivity.this, "PABDIS wants to access your Camera", Toast.LENGTH_LONG).show();
+        } else {
+            ActivityCompat.requestPermissions(VaccinationActivity.this, new String[]{Manifest.permission.CAMERA}, RequestPermissionCode);
+        }
+
+    }
+
+
+
+
+    public static byte[] imageViewToByte(ImageView image) {
+
+        Bitmap bitmap = ((BitmapDrawable)image.getDrawable()).getBitmap();
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byte[] byteArray = stream.toByteArray();
+        return byteArray;
+
     }
 }
