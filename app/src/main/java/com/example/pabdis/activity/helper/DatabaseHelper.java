6c4e96935a97210831de4c8586c7302e74743b45
Jeannie Aquino <jeanniebethdnlx@gmail.com>
2019-08-23 10:04:01 +0800

    Signed-off-by: Jeannie Aquino <jeanniebethdnlx@gmail.com>

--- app/src/main/java/com/example/pabdis/activity/helper/DatabaseHelper.java
+++ app/src/main/java/com/example/pabdis/activity/helper/DatabaseHelper.java
@@ -7,12 +7,19 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final String TAG = DatabaseHelper.class.getSimpleName();
+
+    public static String DB_FILEPATH = "/data/data/{package_name}/databases/database.db";
+
 
     public static final String DATABASE_NAME = "PABDIS.db";
 //    FOR USER LOGIN CREDENTIALS
@@ -238,7 +245,22 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         super(context, DATABASE_NAME, null, 4);
     }
 
+    public boolean importDatabase(String dbPath) throws IOException {
 
+        // Close the SQLiteOpenHelper so it will commit the created empty
+        // database to internal storage.
+        close();
+        File newDb = new File(dbPath);
+        File oldDb = new File(DB_FILEPATH);
+        if (newDb.exists()) {
+            FileUtils.copyFile(new FileInputStream(newDb), new FileOutputStream(oldDb));
+            // Access the copied database so SQLiteHelper will cache it and mark
+            // it as created.
+            getWritableDatabase().close();
+            return true;
+        }
+        return false;
+    }
     @Override
     public void onCreate(SQLiteDatabase db) {
         String CREATE_LOGIN_TABLE = " CREATE TABLE IF NOT EXISTS " + TABLE_USER + "("
