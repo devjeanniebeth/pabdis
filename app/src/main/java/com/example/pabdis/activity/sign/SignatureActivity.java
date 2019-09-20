2c15e24e7e652dedaeac66ada5e10f76cca7b4bf
Jeannie Aquino <jeanniebethdnlx@gmail.com>
2019-09-20 16:31:12 +0800

    Signed-off-by: Jeannie Aquino <jeanniebethdnlx@gmail.com>

---
+++ app/src/main/java/com/example/pabdis/activity/sign/SignatureActivity.java
@@ -0,0 +1,31 @@
+package com.example.pabdis.activity.sign;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.pabdis.R;
+import com.example.pabdis.activity.helper.DatabaseHelper;
+import com.example.pabdis.activity.helper.SessionManager;
+
+public class SignatureActivity extends AppCompatActivity {
+
+
+    
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_petvacc);
+
+
+
+
+    }
+
+
+
+
+}
